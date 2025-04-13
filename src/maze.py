6cb60b409bc3db112e4648286aaea42fbd6b5c96
zakhida1 <zakhida.nasiridinova@alatoo.edu.kg>
2025-04-13 17:53:48 +0600

    Update maze.py

--- src/maze.py
+++ src/maze.py
@@ -1,1 +1,14 @@
+// Maze.java
+import java.util.Random;
 
+public class Maze {
+    private final int rows,cols;
+    public char[][] grid;
+    private final Random rand=new Random();
+
+    public Maze(int rows,int cols) {
+        this.rows = rows % 2 == 0 ? rows + 1 : rows;
+        this.cols=cols % 2 == 0 ? cols + 1 : cols;
+        grid=new char[this.rows][this.cols];
+        initializeMaze();
+    }
