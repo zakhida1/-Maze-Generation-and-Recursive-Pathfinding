c662e47cc126c1be0090f082948d4753547925d4
zakhida1 <zakhida.nasiridinova@alatoo.edu.kg>
2025-04-13 17:52:45 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        int rows =InputHandler.getMazeSize("Enter number of rows (min 10): ");
+        int cols= InputHandler.getMazeSize("Enter number of columns (min 10): ");
+
+        Maze maze = new Maze(rows,cols);
+        maze.generate();
+
+        Solver solver = new Solver(maze);
+        solver.solve();
+
+        MazePrinter.printMaze(maze);
+    }
+}
 
